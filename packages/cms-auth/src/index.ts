d32aae9cea0f7cea76480d8f6824b767ed6812d8
Mike Straczek <mike.straczek@next-levels.de>
2025-07-24 15:45:23 +0200

    chore: bump version to 1.0.8, update TypeScript configuration, and enhance exports in cms-auth

--- packages/cms-auth/src/index.ts
+++ packages/cms-auth/src/index.ts
@@ -1,9 +1,6 @@
-export { createAuthConfig, authConfig } from "./config/auth-config";
-
-export { hashPassword, verifyPassword } from "./lib/password";
-
+export * from "./config/auth-config";
+export * from "./lib/password";
+export * from "./context/auth";
 export * from "./types/user";
-
 export * from "./schemas/user-schema";
-
 export * from "./constants/roles";
